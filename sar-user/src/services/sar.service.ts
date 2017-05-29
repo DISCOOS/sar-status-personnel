95c124ae2407e2448a69d6d5301ad7188a019637
Lasse stasjonær <lasse.aat@gmail.com>
2017-05-29 13:06:04 +0200

    Siste fixes fra merge

--- sar-user/src/services/sar.service.ts
+++ sar-user/src/services/sar.service.ts
@@ -18,14 +18,13 @@ export class SARService {
     loggedIn: boolean;
     token: string;
     available: string;
-    trackable: string;
     id: any;
     missions: Observable<Mission[]>;
     mission: Mission;
     alarm: Alarm;
     user: SARUser;
     // Other components can subscribe to this 
-    //public isLoggedIn: Subject<boolean> = new Subject();
+    public isLoggedIn: Subject<boolean> = new Subject();
 
     constructor(private http: Http) {}
 
@@ -97,7 +96,7 @@ export class SARService {
                     // store user details and token in local storage to keep user logged in between page refreshes
                     localStorage.setItem('currentUser', JSON.stringify(res.user));
                     this.loggedIn = true;
-                    //this.isLoggedIn.next(this.loggedIn);
+                    this.isLoggedIn.next(this.loggedIn);
                 } else {
                     return Observable.throw(new Error("error"));
                 }
@@ -125,7 +124,7 @@ export class SARService {
 
     /**
      * Method to persist SARUser-variable isTrackable to database.
-     * @param isTrackable wanted boolean value for the user.
+     * @param isTrackable boolean value to persist.
      */
 
     public setTrackable(isTrackable: boolean) {
@@ -141,37 +140,6 @@ export class SARService {
             .map((res) => {
                 return res.json();
             })
-    }
-
-    public setTrackable(isTrackable: boolean) {
-
-        let user = this.getUser();
-        let url = baseUrl + "/SARUsers/" + this.getUser().id;
-
-        let options = new RequestOptions({ withCredentials: true })
-
-        //Hack å bare sette hele bodyen sånn, men ellers settes alt annet til null
-        let body = {
-            "kovaId": user.kovaId,
-            "name": user.name,
-            "email": user.email,
-            "phone": user.phone,
-            "isAvailable": user.isAvailable,
-            "isTrackable": isTrackable,
-            "isAdmin": user.isAdmin,
-            "id": user.id,
-            "expenceId": user.expenceId
-        };
-
-        return this.http
-            .patch(url, body, options)
-            .map(res => {
-                console.log(res.json())
-                return res.json()
-            })
-
-        //.catch(this.handleError)
-
     }
 
     /**
